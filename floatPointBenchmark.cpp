a0b44cd81c22dbd6d26b7947e3579d23fd22261d
sjnyth <saujanya99@outlook.com>
2024-03-29 02:43:21 -0400

    init start time and perform arithmetic operations

--- floatPointBenchmark.cpp
+++ floatPointBenchmark.cpp
@@ -1,6 +1,18 @@
 #include <time.h>
 #include <iostream>
 
-int main(){
-    return 0
-}
+int main() {
+    // Measure start time
+    clock_t startTime;
+    startTime = clock();
+    
+    double a = 4.6;
+    double b = 5.4;
+    double result;
+    for (long i = 0; i < 5e9; i++){
+        result = a * b;
+    }
+
+    for (long i = 0; i < 10e9; i++){
+        result = a + b;
+    }
