30ce3efd6566ee579c8f5bdf2c539f4558172578
sjnyth <saujanya99@outlook.com>
2024-03-29 02:47:32 -0400

    calculate elapsed time for double precision operations

--- floatPointBenchmark.cpp
+++ floatPointBenchmark.cpp
@@ -9,10 +9,23 @@ int main() {
     double a = 4.6;
     double b = 5.4;
     double result;
+    // Perform multiplication operation
     for (long i = 0; i < 5e9; i++){
         result = a * b;
     }
-
+    // Perform addition operation
     for (long i = 0; i < 10e9; i++){
         result = a + b;
     }
+    // Perform division operation
+    for (long i = 0; i < 2e9; i++) {
+        result = a / b;
+    }
+
+    // Calculate elapsed time
+    double elapsedTimeInSeconds = (clock() - startTime) / static_cast<double>(CLOCKS_PER_SEC);
+
+    // Print the result
+    std::cout << "Double Operations take ~ " << elapsedTimeInSeconds << "s\n";
+    return 0;
+}
