44c9e32bc2c6a7440d1d02fc53841f8fbc683ae5
sjnyth <saujanya99@outlook.com>
2024-03-29 20:24:37 -0400

    Read operations loop on arrays

--- memoryBenchmark.cpp
+++ memoryBenchmark.cpp
@@ -3,3 +3,13 @@
 int main(){
     clock_t t;
     startTime = clock();
+    // Perform memory read operations
+    for (int i = 0; i < 5e9; i++){
+        int readList[100]{0};
+        int readValue;
+        int readIndex = 0;
+        while (readIndex < 100) {
+            readValue = readList[readIndex];
+            ++readIndex;
+        }
+    }
