65e9c7cd3221d22504f69a2aac02687df27b0c68
sjnyth <saujanya99@outlook.com>
2024-03-29 22:38:25 -0400

    Open 'read.txt', Read Contents Repeatedly, Handle EOF, and Close File Stream

--- harddriveBenchmark2.cpp
+++ harddriveBenchmark2.cpp
@@ -15,3 +15,13 @@ int main() {
         fwrite(array, 1, 10000, writeFile);
     }
     fclose(writeFile);
+
+    FILE* readFile = std::fopen("write.txt", "r");
+    for (long i = 0; i < 100000; i++) {
+        if (feof(readFile)) {
+            fseek(readFile, 0, SEEK_SET);
+        }
+        char array[10000];
+        fread(array, 1, 10000, readFile);
+    }
+    fclose(readFile);
