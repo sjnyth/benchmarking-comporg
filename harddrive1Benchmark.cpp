b8c0837239750b4bc497a47b478e5cfca32337e0
sjnyth <saujanya99@outlook.com>
2024-03-29 21:47:57 -0400

    Write 100-byte array to 'write.txt' iteratively and Read 100 bytes from 'read.txt' with Resetting File Pointer

--- harddrive1Benchmark.cpp
+++ harddrive1Benchmark.cpp
@@ -9,3 +9,16 @@ int main(){
     for (int i = 0; i < 100; i++){
         array[i] = 'a';
     }
+
+    for (long i = 0; i < 10e9; i++){
+        fwrite(array, 1, 100, p);
+    }
+    fclose(p);
+    FILE * p2 = std::fopen("read.txt", "r");    
+    for (long i = 0; i < 10e9; i++){
+        if (feof(p2)){
+            fseek(p2, 0, SEEK_SET);
+        }
+        char array[100];
+        fread(array, 1, 100, p2);
+    }
